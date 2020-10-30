499414b4f0ad6b715adbb5d6c72c572f2d102e84
Chris <chris.demetriad@thecommercepartnership.com>
2020-10-30 21:36:06 +0000

    Fetching initial data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	const getData = async () => {
+		const data = await fetch("http://nyx.vima.ekt.gr:3000/api/books", {
+			method: "POST",
+		});
+		const response = await data.json();
+		// console.log(response.books);
+		setData(response.books);
+	};
+
+	return (
+		<main className="App">
+			<p>Books</p>
+			{data.map((book, index) => (
+				<div key={book.id}>
+					<p>
+						{index + 1} - {book.id}, {book.book_title}
+					</p>
+				</div>
+			))}
+		</main>
+	);
+};
 
 export default App;
