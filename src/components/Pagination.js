4214a6b571cbb8a1714cc35a642a201c442cde07
Chris <chris.demetriad@thecommercepartnership.com>
2020-10-31 02:49:52 +0000

    Refactoring the pagination component

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,9 +2,16 @@ import React from "react";
 
 const Pagination = ({ pages, pageNumber }) => {
 	let content = [];
+	let pagesToBeShown = 4;
+	let page = pageNumber - pagesToBeShown > 0 ? pageNumber - pagesToBeShown : 1;
 
-	for (let i = 1; i <= pages; i++) {
-		if (pageNumber !== i) content.push(<a href={i}>Page {i}</a>);
+	for (let i = 0; i < pagesToBeShown * 2 && page < pages; i++) {
+		content.push(
+			<a key={page} href={page}>
+				Page {page}
+			</a>
+		);
+		page++;
 	}
 
 	return content;
