78da36c802cfe858b90c94b9ed97f1eddb1bc146
Chris <chris.demetriad@thecommercepartnership.com>
2020-11-01 02:10:16 +0000

    Adding search functionality, maintaing state between pages (no routing) with localstorage

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 import Pagination from "./components/Pagination";
 const App = () => {
-	// We can also use URLSearchParamsm but it doesn't work in IE
-	// In a real world scenario I would use
-	// https://github.com/pbeshai/use-query-params, or similar
-
 	let path = window.location.pathname;
 	let pageNumber = path.substr(1);
 
 	const [data, setData] = useState([]);
 	const [pages, setPages] = useState([]);
+	const [search, setSearch] = useState(localStorage.getItem("searchTerm") || "");
 
 	const url = "http://nyx.vima.ekt.gr:3000/api/books";
 
@@ -26,6 +23,7 @@ const App = () => {
 			},
 			body: JSON.stringify({
 				page: pageNumber || 1,
+				filters: [{ type: "all", values: [search] }],
 			}),
 		});
 		const response = await data.json();
@@ -36,13 +34,34 @@ const App = () => {
 		setData(response.books);
 	};
 
+	const handleChange = (event) => {
+		setSearch(event.target.value);
+		localStorage.setItem("searchTerm", event.target.value);
+	};
+
+	const handleSubmit = (event) => {
+		getData(search);
+		window.location.href = "/";
+		event.preventDefault();
+	};
+
+	const clearSearchTerm = () => {
+		setSearch(localStorage.setItem("searchTerm", ""));
+		window.location.href = "/";
+	};
+
 	return (
 		<main className="App">
 			<h3>Books</h3>
+			<p>Search results shown from {search} </p>
+			<button onClick={clearSearchTerm}>Clear search term</button>
+			<form onSubmit={handleSubmit}>
+				<input onChange={handleChange} type="text" placeholder={search} />
+			</form>
 			{data.map((book, index) => (
 				<div key={book.id}>
 					<p>
-						{index + 1} - {book.id}, {book.book_title}
+						{index + 1} - {book.id}, {book.book_title}, {book.book_pages} pages, published in {book.book_publication_year} in {book.book_publication_city}, {book.book_publication_country}
 					</p>
 				</div>
 			))}
